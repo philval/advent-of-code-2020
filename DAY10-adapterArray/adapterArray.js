feca25366101499e4b4ae9c1fbe25973d253ceea
philval <philip.valentino@mac.com>
2021-10-19 22:57:45 +0100

    Solution for DAY10 Part 1

--- DAY10-adapterArray/adapterArray.js
+++ DAY10-adapterArray/adapterArray.js
@@ -1,32 +1,48 @@
-// OUTPUT ratings
-import { data } from "./adapterArrayData.js"
+// STRATEGY PART ONE
 
-console.log(data)
-
-// STRATEGY
 // three "objects":
 // OUTLET 0 jolts
-// ADAPTER
+// ADAPTERS
 // DEVICE = highest rated ADAPTER + 3 jolts
 
 // adapters have "output ratings" === puzzle input
-// adapter can take input from other adaptors outputs
-// adapter INPUT are 1,2,OR 3 lower than OUTPUT rating ( make a choice how ? )
+// adapters can take input from other adaptors outputs
+// adapters INPUT are 1,2,OR 3 lower than OUTPUT rating ( make a choice how ? )
 
 // "If you use every adapter in your bag at once",
 // "in a chain that uses all of the adapters",
-// so we must do that, USE EVERY ADAPTER ONCE
+// so we must do that, USE EVERY ADAPTER ONCE, a hint...
 
-// "However, in order to not skip any adapters"
-// path finding problem ? AoC calls it a chain
-// start of the path is the outlet, jolts out = 0
+// need to always pick lowest rating for next adapter when there are several adapters
+// insight is to sort the adapters!
+// then calc diff versus previous adapter ( 1 or 3 ) and count diffs
 
-// need to capture number of jolt diffs = object ?
+// dont forget DEVICE rating calc last.
 
-// DONT FORGET Device rating calc last.
+// FINALLY What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?
 
-// What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?
+import { data } from "./adapterArrayData.js"
 
-const DEVICE = Math.max(data.split("\n")) + 3
+const parsedData = data.split("\n").map((n) => Number.parseInt(n, 10))
 
-console.log(DEVICE)
+const outletRating = 0
+
+const deviceRating = Math.max(...parsedData) + 3
+
+const chain = parsedData.sort((a, b) => a - b)
+
+chain.unshift(outletRating)
+
+chain.push(deviceRating)
+
+// accumulators
+let diff1 = 0
+let diff3 = 0
+
+for (let i = 0; i < chain.length; i++) {
+  if (chain[i + 1] - chain[i] === 1) diff1++
+  if (chain[i + 1] - chain[i] === 3) diff3++
+}
+
+// 1690
+console.log("Day 10: Adapter Array Part 1 ", diff1 * diff3)
